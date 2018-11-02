f52da5950b5a51eb3a2222d129ec8919f64d9f6c
Kernael92 <kernaelapuko5@gmail.com>
2018-11-02 15:21:51 +0300

    create a new instance of user class and a test to check if all the objects are instantiated correctly

--- user_credential.py
+++ user_credential.py
@@ -4,7 +4,7 @@ class User:
     '''
     Class to create user account and store their information
     '''
-    user_list = [] #Empty user list
+    users_list = [] #Empty users list
 def __init__(self,first_name,last_name,password): 
     '''
     --init__ method that helps us define properties for our objects.
@@ -13,4 +13,9 @@ def __init__(self,first_name,last_name,password):
     self.first_name = first_name
     self.last_name = last_name
     self.password = password
+class Credential:
+    '''
+    Class to create user account credentials, generate passwords and store their information.
+    '''
+    credentials_list = [] #Empty credentials list
 
