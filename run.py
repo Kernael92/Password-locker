8feba6ea83e91d6c1e6089e886a47654d193e330
Kernael92 <kernaelapuko5@gmail.com>
2018-11-04 18:29:21 +0300

    create a main function that calls all other functions

--- run.py
+++ run.py
@@ -5,6 +5,13 @@ import string
 import random
 import pyperclip
 
+def create_credential(user_name,site_name,account_name,password):
+    '''
+    Function that create a new credential
+    '''
+    new_credential = Credential(user_name,site_name,account_name,password)
+    return new_credential
+
 def  save_user(user):
         '''
         Function to save user
@@ -45,3 +52,79 @@ def check_user(current_user):
         Function that checks if the name and password entered match entries in the users_list
         '''
         return current_user
+
+def main():
+    print("Hello, Welcome to your credential list. What is your name?")
+    user_name = input()
+
+    print(f"Hello {user_name}. what would you like to do?")
+    print('\n')
+
+    while True:
+            print("Use these short codes : cc - create a new credential, dc - display credentials, fc -find a credential, ex -exit the credential list ")
+
+            short_code = input().lower()
+
+            if short_code == 'cc':
+                    print("New Credential")
+                    print("-"*10)
+
+                    print ("User name ....")
+                    user_name = input()
+
+                    print("Site name ...")
+                    site_name = input()
+
+                    print("Account name ...")
+                    account_name = input()
+
+                    print("password ...")
+                    password = input()
+
+        
+
+
+                    save_credential(create_credential(user_name,site_name,account_name,password)) # create and save new credential.
+                    print ('\n')
+                    print(f"New credential {user_name} {site_name} {account_name} {password}created")
+                    print ('\n')
+
+            elif short_code == 'dc':
+
+                    if display_credentials():
+                            print("Here is a list of all your credentials")
+                            print('\n')
+
+                            for credential in display_credentials():
+                                    print(f"{credential.user_name} {credential.site_name} {credential.account}.....{credential.password}")
+
+                            print('\n')
+                    else:
+                            print('\n')
+                            print("You dont seem to have any credentials saved yet")
+                            print('\n')
+
+            elif short_code == 'fc':
+
+                    print("Enter the name of the site you want to search for")
+
+                    search_site_name = input()
+                    if credential_exist(search_site_name):
+                            search_credential = find_credential(search_site_name)
+                            print(f"{search_credential.user_name} {search_credential.site_name} {search_credential.account_name}")
+                            print('-' * 20)
+
+                            print(f"site_name.......{search_credential.site_name}")
+                            print(f"account name.......{search_credential.account_name}")
+                    else:
+                            print("That credential does not exist")
+
+            elif short_code == "ex":
+                    print("Bye .......")
+                    break
+            else:
+                    print("I really didn't get that. Please use the short codes")
+if __name__ == '__main__':
+    main()
+
+
