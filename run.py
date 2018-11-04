7b4612907a1188302987a98a77789d68a12ed74d
Kernael92 <kernaelapuko5@gmail.com>
2018-11-04 17:11:05 +0300

    create a function that calls in the user object and then calls the save_user method to save the contact.

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3.7
+from user_credential import User 
+from credentials import Credential
+def  save_user(user):
+        '''
+        Function to save user
+        '''
+        user.save_user()
